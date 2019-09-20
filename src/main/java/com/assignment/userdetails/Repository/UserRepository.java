0a4bf8246c6ff918724d3683a5685be4fdd708fc
shubhamsinghhm <55501404+shubhamsinghhm@users.noreply.github.com>
2019-09-20 21:36:12 +0530

    Add files via upload

--- src/main/java/com/assignment/userdetails/Repository/UserRepository.java
+++ src/main/java/com/assignment/userdetails/Repository/UserRepository.java
@@ -1,26 +1,86 @@
 package com.assignment.userdetails.Repository;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.assignment.userdetails.Pojo.User;
 
-@Repository
-@Transactional
-public interface UserRepository extends CrudRepository<User,Long>{
-	 @Query(value = "SELECT * FROM user u WHERE u.username =?1",
-	            nativeQuery=true
-	    )
-	    public User getByName(String username);
-	 
+/*import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
 
-	<S extends User> S saveAndFlush(S user);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.assignment.userdetails.Pojo.User;*/
+
+
+/*@Repository
+public class UserRepository {
+	 @Autowired
+	 private EntityManager entityManager;
+	 
+	 public UserRepository(){
+	 }
+    
+	 @Transactional
+	 public void save(User user) {
+
+		entityManager.persist(user);
+		
+	}
+     @Transactional
+	 public User getByName(String username) throws NoResultException{
+		User user=(User) entityManager.createQuery("Select u from User u where u.username like :UserName").setParameter("UserName",username).getSingleResult();
+		return user;
+	}
+     
+     @Transactional
+	 @SuppressWarnings("unchecked")
+	 public Iterable<User> findAll() throws NoResultException {
+		List<User> list=entityManager.createQuery("Select u from User u",User.class).getResultList();
+		return list;
+	}
+	 
 
 
 	
-	 
+}*/
 
+//This is static repository created within the class
+@Repository
+public class UserRepository{
+	
+    private static ArrayList<User> list=new ArrayList<User>();
+    static{
+    	list.add(new User("Shu12","shubham","singh",25,"student","delhi"));
+    	list.add(new User("ram12","ram","singh",26,"doctor","banglore"));
+    }
+    
+     public void save(User user) {
+		for(User userfromlist:list){
+		if(user.getUsername().equals(userfromlist.getUsername())){
+			return;
+		   }
+		}
+		list.add(user);
+		   }
+
+       public User getByName(String username) {
+		
+		 for(User userfromlist:list){
+			if(userfromlist.getUsername().equals(username)){
+				return userfromlist;
+			     }
+			}
+		return null;
+	                      }
+
+     public Iterable<User> findAll() {
+		
+		return list;
+	  }
+	
 }
